2db2a37f458e10af3e9f715b76f818664650bb55
AmayaVS <amayavammu@gmail.com>
2022-04-30 15:05:11 +0530

    UI page commit part1

---
+++ app/src/main/java/com/example/parkingareadisplay/closing_door.java
@@ -0,0 +1,14 @@
+package com.example.parkingareadisplay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class closing_door extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_closing_door);
+    }
+}
